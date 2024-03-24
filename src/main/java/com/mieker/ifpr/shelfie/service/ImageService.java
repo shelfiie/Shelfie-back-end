73e47b2d7c2799cb83cb6056949322284e8555f5
Gabriela Marques <gabriela2013.ms@gmail.com>
2024-03-24 18:32:01 -0300

    UPD teste

--- src/main/java/com/mieker/ifpr/shelfie/service/ImageService.java
+++ src/main/java/com/mieker/ifpr/shelfie/service/ImageService.java
@@ -1,4 +1,9 @@
 package com.mieker.ifpr.shelfie.service;
 
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
 public class ImageService {
 }
