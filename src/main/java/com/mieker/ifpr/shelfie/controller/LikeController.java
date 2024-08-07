6f79fbed161a3af088a1758e61ba568e38413620
Gabriela Marques <gabriela2013.ms@gmail.com>
2024-08-07 00:33:52 -0300

    feat: rotas do review like

--- src/main/java/com/mieker/ifpr/shelfie/controller/LikeController.java
+++ src/main/java/com/mieker/ifpr/shelfie/controller/LikeController.java
@@ -1,5 +1,7 @@
 package com.mieker.ifpr.shelfie.controller;
 
+import com.mieker.ifpr.shelfie.dto.Like.LikeDTO;
+import com.mieker.ifpr.shelfie.dto.Like.UserLikeDTO;
 import com.mieker.ifpr.shelfie.dto.MyBooks.MyBooksDTO;
 import com.mieker.ifpr.shelfie.entity.enumeration.BookStatus;
 import com.mieker.ifpr.shelfie.service.FavoriteBookService;
@@ -10,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
+import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
@@ -27,4 +30,31 @@ public class LikeController {
         return ResponseEntity.status(201).body(message);
     }
 
+    @PreAuthorize("isAuthenticated()")
+    @DeleteMapping("{reviewId}")
+    public ResponseEntity<String> deslikeReview(@PathVariable UUID reviewId) {
+        String message = likeService.delete(reviewId);
+        return ResponseEntity.status(200).body(message);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("{reviewId}")
+//    nessa da pra retornar o nome do usuario
+    public ResponseEntity<List<LikeDTO>> getReviewLikes(@PathVariable UUID reviewId) {
+        List<LikeDTO> likeDTOList = likeService.getReviewLikes(reviewId);
+        return ResponseEntity.status(200).body(likeDTOList);
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/mine")
+    public ResponseEntity<List<UserLikeDTO>> getUserLikes() {
+        List<UserLikeDTO> userLikeDTOList = likeService.getUserLikes();
+        return ResponseEntity.status(200).body(userLikeDTOList);
+    }
+
+//    @GetMapping("/user/{userId}")
+//    public ResponseEntity<List<UserLikeDTO>> getUserLikes(@PathVariable UUID reviewId) {
+//        String message = likeService.create(reviewId);
+//        return ResponseEntity.status(201).body(message);
+//    }
 }
--- src/main/java/com/mieker/ifpr/shelfie/service/LikeService.java
+++ src/main/java/com/mieker/ifpr/shelfie/service/LikeService.java
@@ -1,6 +1,8 @@
 package com.mieker.ifpr.shelfie.service;
 
 import com.mieker.ifpr.shelfie.config.Validation;
+import com.mieker.ifpr.shelfie.dto.Like.LikeDTO;
+import com.mieker.ifpr.shelfie.dto.Like.UserLikeDTO;
 import com.mieker.ifpr.shelfie.entity.Book;
 import com.mieker.ifpr.shelfie.entity.Like;
 import com.mieker.ifpr.shelfie.entity.Review;
@@ -12,6 +14,7 @@ import com.mieker.ifpr.shelfie.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -33,5 +36,33 @@ public class LikeService {
         like.setUser(user);
         likeRepository.save(like);
         return "Review curtida com sucesso!";
+    }
+
+    public List<LikeDTO> getReviewLikes(UUID reviewId) {
+        List<Like> likeList = likeRepository.findByReviewId(reviewId);
+        return likeList.stream().map(like -> {
+            LikeDTO likeDTO = new LikeDTO();
+            likeDTO.setUserId(like.getUser().getId());
+            likeDTO.setNickname(like.getUser().getNickname());
+            return likeDTO;
+        }).toList();
+    }
+
+    public String delete(UUID reviewId) {
+        UUID userId = userValidation.userAuthenticator();
+        Optional<Like> like = likeRepository.findByReviewId(reviewId).stream().filter(l -> l.getUser().getId().equals(userId)).findFirst();
+        like.ifPresent(likeRepository::delete);
+        return "Review descurtida com sucesso!";
+    }
+
+    public List<UserLikeDTO> getUserLikes() {
+        UUID userId = userValidation.userAuthenticator();
+        List<Like> likeList = likeRepository.findAll();
+        return likeList.stream().filter(like -> like.getUser().getId().equals(userId)).map(like -> {
+            UserLikeDTO userLikeDTO = new UserLikeDTO();
+            userLikeDTO.setReviewId(like.getReview().getId());
+            userLikeDTO.setBookId(like.getReview().getMyBooks().getBook().getId());
+            return userLikeDTO;
+        }).toList();
     }
 }
