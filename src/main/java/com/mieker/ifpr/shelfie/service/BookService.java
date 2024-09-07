6138264c9fe051c617e02c119a0cffcf0c271d6d
Gabriela Marques <gabriela2013.ms@gmail.com>
2024-09-07 18:54:16 -0300

    cln: deleted files that weren't used

--- src/main/java/com/mieker/ifpr/shelfie/service/BookService.java
+++ src/main/java/com/mieker/ifpr/shelfie/service/BookService.java
@@ -2,8 +2,8 @@ package com.mieker.ifpr.shelfie.service;
 
 import com.mieker.ifpr.shelfie.dto.Book.BookDTO;
 import com.mieker.ifpr.shelfie.entity.Book;
-import com.mieker.ifpr.shelfie.entity.enumeration.BookStatus;
 import com.mieker.ifpr.shelfie.exception.IdNotFoundException;
+import com.mieker.ifpr.shelfie.exception.NotFoundException;
 import com.mieker.ifpr.shelfie.mapper.BookMapper;
 import com.mieker.ifpr.shelfie.repository.BookRepository;
 import lombok.AllArgsConstructor;
@@ -20,35 +20,25 @@ public class BookService {
     private final BookMapper bookMapper;
     private final BookApiService bookApiService;
 
-    public Book createBook(String googleId) throws ParseException {
+    public Book createBook(String googleId) {
         Book book = bookRepository.findByGoogleId(googleId).orElse(null);
         if (book != null) {
             return book;
         } else {
             BookDTO bookDTO = bookApiService.getBookByGoogleId(googleId);
             book = bookMapper.bookDTOtoBook(bookDTO);
-            System.out.println(book);
             return bookRepository.save(book);
         }
     }
 
     public BookDTO getBookByGoogleId(String googleId) {
-        Book book = bookRepository.findByGoogleId(googleId).orElseThrow(() -> new IdNotFoundException("Book not found with googleId: " + googleId));
+        Book book = bookRepository.findByGoogleId(googleId).orElseThrow(() -> new NotFoundException("Livro não encontrado com esse ID:" + googleId));
         return bookMapper.bookToBookDTO(book);
     }
 
     public BookDTO getBookById(UUID id) {
-        Book book = bookRepository.findById(id).orElseThrow(() -> new IdNotFoundException("MyBooks not found with id: " + id));
+        Book book = bookRepository.findById(id).orElseThrow(() -> new NotFoundException("Livro não encontrado com esse:" + id));
         return bookMapper.bookToBookDTO(book);
-    }
-
-    public boolean isBookStatusValid(String status) {
-        try {
-            BookStatus.valueOf(status);
-            return true; // O valor é válido
-        } catch (IllegalArgumentException ex) {
-            return false; // O valor não é válido
-        }
     }
 
     public List<Book> getAllBooks() {
